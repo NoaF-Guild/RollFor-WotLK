d832c58741e6a26897b483aeb8f3b00fc60da55d
kff <kff@users.noreply.github.com>
2026-05-25 22:20:00 -0400

    fix(wotlk): replace this/arg1 with self in entry_update and scroll/tab/area handlers

--- src/OptionsGuiElements.lua
+++ src/OptionsGuiElements.lua
@@ -53,27 +53,28 @@ function M.create_gui_entry( title, frames, populate )
   end
 end
 
-function M.entry_update()
+function M.entry_update( self )
+  if m.vanilla then self = this end
   local focus = m.api.GetMouseFocus()
   if (focus and focus.value) then
     return
   end
 
-  if m.api.MouseIsOver( this ) and not this.over then
-    this.tex:Show()
-    this.over = true
-    if this:GetParent():GetParent():GetParent():GetParent():GetParent().show_help then
-      if this.tooltip then
-        this:GetParent().tooltip = this
-        m.api.GameTooltip:SetOwner( this, "ANCHOR_TOPLEFT" )
-        m.api.GameTooltip:SetText( this.tooltip )
+  if m.api.MouseIsOver( self ) and not self.over then
+    self.tex:Show()
+    self.over = true
+    if self:GetParent():GetParent():GetParent():GetParent():GetParent().show_help then
+      if self.tooltip then
+        self:GetParent().tooltip = self
+        m.api.GameTooltip:SetOwner( self, "ANCHOR_TOPLEFT" )
+        m.api.GameTooltip:SetText( self.tooltip )
         m.api.GameTooltip:Show()
       end
     end
-  elseif not m.api.MouseIsOver( this ) and this.over then
-    this.tex:Hide()
-    this.over = nil
-    if m.api.GameTooltip:IsShown() and this:GetParent().tooltip == this then
+  elseif not m.api.MouseIsOver( self ) and self.over then
+    self.tex:Hide()
+    self.over = nil
+    if m.api.GameTooltip:IsShown() and self:GetParent().tooltip == self then
       m.api.GameTooltip:Hide()
     end
   end
@@ -139,10 +140,10 @@ function M.create_scroll_frame( parent, name )
   f.slider.thumb:SetHeight( 50 )
   f.slider.thumb:SetTexture( .125, .624, .976, .5 )
 
-  f.slider:SetScript( "OnValueChanged", function()
+  f.slider:SetScript( "OnValueChanged", function( self )
     if is_updating then return end
     is_updating = true
-    f:SetVerticalScroll( this:GetValue() )
+    f:SetVerticalScroll( self:GetValue() )
     -- update thumb size but do NOT call update_scroll_state (avoids re-entry)
     local scroll_range = f:GetVerticalScrollRange()
     local v = f:GetHeight()
@@ -210,8 +211,8 @@ function M.create_scroll_frame( parent, name )
   end
 
   f:EnableMouseWheel( 1 )
-  f:SetScript( "OnMouseWheel", function()
-    this:scroll( arg1 * 10 )
+  f:SetScript( "OnMouseWheel", function( self, delta )
+    self:scroll( delta * 10 )
   end )
 
   return f
@@ -227,8 +228,8 @@ function M.create_scroll_child( parent, name )
 
   parent:SetScrollChild( f )
 
-  f:SetScript( "OnUpdate", function()
-    this:GetParent():update_scroll_state()
+  f:SetScript( "OnUpdate", function( self )
+    self:GetParent():update_scroll_state()
   end )
 
   return f
@@ -242,16 +243,16 @@ function M.create_tab_frame( parent, title )
   f:SetPoint( "BOTTOMRIGHT", parent.tab_area, "TOPLEFT", (parent.tab_area.count + 1) * 65, -20 )
   f.parent = parent
 
-  f:SetScript( "OnClick", function()
-    if this.area:IsShown() then
+  f:SetScript( "OnClick", function( self )
+    if self.area:IsShown() then
       return
     else
-      for id, name in pairs( this.parent ) do
+      for id, name in pairs( self.parent ) do
         if type( name ) == "table" and name.area and id ~= "parent" then
           name.area:Hide()
         end
       end
-      this.area:Show()
+      self.area:Show()
     end
   end )
 
@@ -278,17 +279,17 @@ function M.create_area( parent, title, func )
   f.bg:SetTexture( 1, 1, 1, .05 )
   f.bg:SetAllPoints()
 
-  f:SetScript( "OnShow", function()
+  f:SetScript( "OnShow", function( self )
     parent.active_area = title
-    this.indexed = true
-    this.button.text:SetTextColor( 0.1254, 0.6235, 0.9764, 1 )
-    this.button.bg:SetTexture( 1, 1, 1, 1 )
-    this.button.bg:SetGradientAlpha( "VERTICAL", 1, 1, 1, .05, 0, 0, 0, 0 )
+    self.indexed = true
+    self.button.text:SetTextColor( 0.1254, 0.6235, 0.9764, 1 )
+    self.button.bg:SetTexture( 1, 1, 1, 1 )
+    self.button.bg:SetGradientAlpha( "VERTICAL", 1, 1, 1, .05, 0, 0, 0, 0 )
   end )
 
-  f:SetScript( "OnHide", function()
-    this.button.text:SetTextColor( 1, 1, 1, 1 )
-    this.button.bg:SetTexture( 0, 0, 0, 0 )
+  f:SetScript( "OnHide", function( self )
+    self.button.text:SetTextColor( 1, 1, 1, 1 )
+    self.button.bg:SetTexture( 0, 0, 0, 0 )
   end )
 
   if func then
@@ -298,11 +299,11 @@ function M.create_area( parent, title, func )
     ---@class ChildFrame
     f.scroll.content = M.create_scroll_child( f.scroll )
     f.scroll.content.parent = f.scroll
-    f.scroll.content:SetScript( "OnShow", function()
-      this.parent:UpdateScrollChildRect()
-      if not this.setup then
-        func()
-        this.setup = true
+    f.scroll.content:SetScript( "OnShow", function( self )
+      self.parent:UpdateScrollChildRect()
+      if not self.setup then
+        func( self )
+        self.setup = true
       end
     end )
   end
